b74b3967602d8594f6b161040d3f51c37d48af47
Juhasz Bence <juhbence90@gmail.com>
2026-01-07 21:08:16 +0100

    Meta, Tanstack Query added

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,22 +1,44 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
+import {
+  HeadContent,
+  Outlet,
+  createRootRouteWithContext,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import { QueryClient } from "@tanstack/react-query";
 
-export const Route = createRootRoute({
+type RouterContext = {
+  queryClient: QueryClient;
+};
+
+export const Route = createRootRouteWithContext<RouterContext>()({
+  head: () => ({
+    meta: [
+      {
+        name: "description",
+        content:
+          "Share, explore and build on the best startup ideas and side hustles",
+      },
+      {
+        title: "IdeaDrop - Your Idea Hub",
+      },
+    ],
+  }),
   component: () => (
     <>
+      <HeadContent />
       <Outlet />
       <TanStackDevtools
         config={{
-          position: 'bottom-right',
+          position: "bottom-right",
         }}
         plugins={[
           {
-            name: 'Tanstack Router',
+            name: "Tanstack Router",
             render: <TanStackRouterDevtoolsPanel />,
           },
         ]}
       />
     </>
   ),
-})
+});
---
+++ src/routes/ideas/$ideaId/index.tsx
@@ -0,0 +1,39 @@
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
+import type { Idea } from "@/types";
+
+const fetchIdea = async (ideaId: string): Promise<Idea> => {
+  const res = await fetch(`/api/ideas/${ideaId}`);
+
+  if (!res.ok) throw new Error("Failed to fetch data");
+
+  return res.json();
+};
+
+const ideaQueryOptions = (ideaId: string) =>
+  queryOptions({
+    queryKey: ["idead", ideaId],
+    queryFn: () => fetchIdea(ideaId),
+  });
+
+export const Route = createFileRoute("/ideas/$ideaId/")({
+  component: IdeaDetailsPage,
+  loader: async ({ params, context: { queryClient } }) => {
+    return queryClient.ensureQueryData(ideaQueryOptions(params.ideaId));
+  },
+});
+
+function IdeaDetailsPage() {
+  const { ideaId } = Route.useParams();
+  const { data: idea } = useSuspenseQuery(ideaQueryOptions(ideaId));
+
+  return (
+    <div className="p-4">
+      <Link to="/ideas" className="text-blue-500 underline block mb-4">
+        Back To Ideas
+      </Link>
+      <h2 className="text-2xl font-bold">{idea.title}</h2>
+      <p className="mt-2">{idea.description}</p>
+    </div>
+  );
+}
