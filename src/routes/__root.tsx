6ee76a1e2aa5c4cbe527611516741c326a894221
Juhasz Bence <juhbence90@gmail.com>
2026-01-09 18:48:42 +0100

    Header, Layout, Not found page added

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,6 +6,8 @@ import {
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import { QueryClient } from "@tanstack/react-query";
+import Header from "@/components/Header";
+import { Link } from "@tanstack/react-router";
 
 type RouterContext = {
   queryClient: QueryClient;
@@ -24,10 +26,20 @@ export const Route = createRootRouteWithContext<RouterContext>()({
       },
     ],
   }),
-  component: () => (
-    <>
+  component: RootLayout,
+  notFoundComponent: NotFound,
+});
+
+function RootLayout() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col">
       <HeadContent />
-      <Outlet />
+      <Header />
+      <main className="flex justify-center p-6">
+        <div className="w-full max-w-4xl bg-white rounded-2xl shadow-lg p-8">
+          <Outlet />
+        </div>
+      </main>
       <TanStackDevtools
         config={{
           position: "bottom-right",
@@ -39,6 +51,23 @@ export const Route = createRootRouteWithContext<RouterContext>()({
           },
         ]}
       />
-    </>
-  ),
-});
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center text-center py-20">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">404</h1>
+      <p className="text-lg text-gray-600 mb-6">
+        Ooops! The page you are looking for doeas not exist.
+      </p>
+      <Link
+        to="/"
+        className="px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition"
+      >
+        Go Back Home
+      </Link>
+    </div>
+  );
+}
