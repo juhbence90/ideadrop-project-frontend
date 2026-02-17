82679c9b833e7364a8faf3a2baf511ea67a65beb
Juhasz Bence <juhbence90@gmail.com>
2026-02-17 22:39:36 +0100

    Unused Link removed

--- src/routes/ideas/index.tsx
+++ src/routes/ideas/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link } from '@tanstack/react-router';
+import { createFileRoute } from '@tanstack/react-router';
 import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
 import { fetchIdeas } from '@/api/ideas';
 import IdeaCard from '@/components/IdeaCard';
